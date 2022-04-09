87f3cea06130f52ae3990e5075ffbbf202462bf7
Ryan Cartwright <ryan.cartwright@nitric.io>
2022-04-09 17:15:17 +1000

    Add database and basic api

--- api/server.js
+++ api/server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import mysql from 'mysql';
+import cors from 'cors';
+
+const app = express()
 
 // Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
+// Database
 const con = mysql.createConnection({
-  host: "localhost",
+  host: "db",
+  port: "3306",
   user: "root",
   password: "admin",
   database: "bcf",
@@ -15,10 +20,42 @@ const con = mysql.createConnection({
 con.connect();
 
 // App
-const app = express();
+app.use(cors())
+
 app.get('/', (req, res) => {
-  con.query("SELECT * FROM ")
+  con.query("SELECT * FROM products", (err, values) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    res.json(values);
+  });
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.get('/:name', (req, res) => {
+  const { name } = req.params;
+
+  const product = con.query(`SELECT * FROM products WHERE name=${name}`);
+
+  res.json(product)
+})
+
+app.listen(PORT, HOST, () => {
+  con.query(`CREATE TABLE IF NOT EXISTS products(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    stock INT
+  )`);
+
+  con.query(`INSERT IGNORE INTO products (name, description, stock) VALUES ("Wire Tap", "Will wire tap a home", 10)`);
+  con.query(`INSERT IGNORE INTO products (name, description, stock) VALUES ("Root Kit", "Break into any public facing server", 100)`);
+  con.query(`INSERT IGNORE INTO products (name, description, stock) VALUES ("Death USB", "A USB that will cause imminent death", 5)`);
+  con.query(`INSERT IGNORE INTO products (name, description, stock) VALUES ("Password Cracker", "A USB that will cause imminent death", 5)`);
+  con.query(`INSERT IGNORE INTO products (name, description, stock) VALUES ("Pineapple", "A USB that will cause imminent death", 5)`);
+  con.query(`INSERT IGNORE INTO products (name, description, stock) VALUES ("Rubber Ducky", "A USB that will cause imminent death", 5)`);
+  con.query(`INSERT IGNORE INTO products (name, description, stock) VALUES ("Wireless Adaptor", "A USB that will cause imminent death", 5)`);
+  con.query(`INSERT IGNORE INTO products (name, description, stock) VALUES ("RF Receiver", "A USB that will cause imminent death", 5)`);
+
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
---
+++ web/scripts/index.js
@@ -0,0 +1,41 @@
+window.onload = function load() {
+  populateProducts();
+}
+
+const testProducts = [
+  {
+    name: "Wire Tap",
+    description: "",
+    stock: 2,
+  }
+]
+
+async function populateProducts() {
+  const galleryRef = document.getElementById("products");
+
+  const resp = await fetch("http://localhost:3000/");
+  console.log(resp);
+
+  const products = await resp.json();
+  console.log(products);
+
+  products.forEach((product) => {
+    const newProductRef = document.createElement("div");
+    newProductRef.className = "product";
+
+    // Create and append the image
+    const productImage = document.createElement("img");
+    productImage.src = `images/products/fishing-rod.jpeg` //${nameToSlug(product.name)}`;
+    productImage.className = "product-image";
+    newProductRef.appendChild(productImage);
+
+    // Create and append the name
+    const productName = document.createElement("h3");
+    productName.innerHTML = product.name;
+    newProductRef.appendChild(productName);
+
+    galleryRef.appendChild(newProductRef);
+  });
+}
+
+const nameToSlug = (name) => name.toLowerCase().replace(' ', '-');
